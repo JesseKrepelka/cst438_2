0db502d4283f78e6a8af44a48b6ce0a833f915e6
JesseKrepelka <jessekrepelka@gmail.com>
2021-05-10 11:35:20 -0700

    Initial commit

---
+++ src/test/java/cst438_2/Cst4382ApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst4382ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
